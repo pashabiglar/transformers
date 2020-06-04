e777929a849a38cc6259ed919fbff526813536e9
mithun <mithunpaul08@gmail.com>
2020-06-03 21:10:44 -0700

    was able to run training and eval top to bottom. next try running snli?replace mnli with fever+fnc code.

--- run_scripts/run_glue.sh
+++ run_scripts/run_glue.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-export GLUE_DIR="/Users/mordor/research/huggingface/src/transformers/data/datasets/MNLI"
+export GLUE_DIR="/Users/mordor/research/huggingface/src/transformers/data/datasets/"
 export TASK_NAME=MNLI
 
 python ../examples/text-classification/run_glue.py \
     --model_name_or_path bert-base-uncased \
-    --task_name $TASK_NAME \
+    --task_name MNLI \
     --do_train \
     --do_eval \
-    --data_dir $GLUE_DIR/$TASK_NAME \
+    --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/MNLI/ \
     --max_seq_length 128 \
     --per_device_eval_batch_size=8   \
     --per_device_train_batch_size=8   \
     --learning_rate 2e-5 \
     --num_train_epochs 3.0 \
-    --output_dir /tmp/$TASK_NAME/
+    --output_dir /tmp/MNLI/
