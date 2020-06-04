785835a5d32ee8342e2272aa3e74e5c79630cd81
mithun <mithunpaul08@gmail.com>
2020-06-03 21:23:55 -0700

    QNLI is a bad choice, has only 2 labels. should try MNLI itself, but hide dev from doing mismatched and matched divisions

--- run_scripts/run_glue.sh
+++ run_scripts/run_glue.sh
@@ -15,6 +15,6 @@ python ../examples/text-classification/run_glue.py \
     --num_train_epochs 3.0 \
     --output_dir /tmp/MNLI/
 
-
+ 
 #version for edit configurations in pycharm
 # --model_name_or_path bert-base-uncased     --task_name QNLI      --do_train     --do_eval      --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/QNLI/      --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir /tmp/QNLI/
