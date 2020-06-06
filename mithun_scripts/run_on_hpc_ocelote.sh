ec6b2726be263b12b5d0253b6a06b4377288c918
mithun <mithunpaul08@gmail.com>
2020-06-05 22:37:58 -0700

    adding ocelote script v1. toy data

---
+++ mithun_scripts/run_on_hpc_ocelote.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Your job will use 1 node, 28 cores, and 168gb of memory total.
+#PBS -q standard
+#PBS -l select=1:ncpus=28:mem=168gb:pcmem=8gb:ngpus=1
+### Specify a name for the job
+#PBS -N 1_contextual
+### Specify the group name
+#PBS -W group_list=msurdeanu
+### Used if job requires partial node only
+#PBS -l place=pack:exclhost
+### CPUtime required in hhh:mm:ss.
+### Leading 0's can be omitted e.g 48:0:0 sets 48 hours
+#PBS -l cput=1344:00:00
+### Walltime is how long your job will run
+#PBS -l walltime=1:00:00
+#PBS -oe /home/u11/mithunpaul/xdisk/huggingface/hpc_errors_outputs/
+
+#####module load cuda80/neuralnet/6/6.0
+#####module load cuda80/toolkit/8.0.61
+module load singularity/2/2.6.1
+
+cd $PBS_O_WORKDIR
+
+export MODEL_DIR=/xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/BERT_QA/bert_latest/BERT_base_uncased
+export GLUE_DIR=/xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/bert_input_files/FNC/in-domain/lex
+export OUT_DIR=/xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/pretrained_bert_output_models/FEVER/cross-domain/lex/lex-64/e-5
+
+export TRAINED_MODEL=/xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/bert_output_models/FEVER/cross-domain/lex/lex-64/e-5
+export OUT_DIR_2=/xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/bert_output_models/FEVER/cross-domain/train-fever-test-fnc/lex/seq-64/e-5/test
+#train
+#eval
+singularity run --nv /xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/ocelote_BERT_singularity.img /xdisk/msurdeanu/mithunpaul/BERT_REPLICATION/BERT_QA/bert_latest/bert/run_classifier_ARC_DETAILED_sandeep.py --task_name=fevercd --do_predict=true --data_dir=$GLUE_DIR --vocab_file=$MODEL_DIR/vocab.txt --bert_config_file=$MODEL_DIR/bert_config.json --init_checkpoint=$TRAINED_MODEL --max_seq_length=64 --output_dir=$OUT_DIR_2 --do_lower_case=true
