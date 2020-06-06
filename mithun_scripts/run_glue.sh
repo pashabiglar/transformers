112f550d6c6869bc16d44711384a8acfd08c88ba
mithun <mithunpaul08@gmail.com>
2020-06-06 12:14:58 -0700

    python 3.6 inside runglue

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -6,7 +6,7 @@ export GLUE_DIR="../src/transformers/data/datasets/fever/feverindomain/lex/"
 export TASK_NAME=feverindomain
 
 
-python3 ../examples/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval      --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 5.0      --output_dir /tmp/$TASK_NAME --overwrite_output_dir
+python3.6 ../examples/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval      --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 5.0      --output_dir /tmp/$TASK_NAME --overwrite_output_dir
 
 
 # expanded run parameters for feverindomain
