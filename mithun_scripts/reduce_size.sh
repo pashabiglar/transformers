bca9a8f0f4a50875bf8a5f0b643e2537c89d0986
mithun <mithunpaul08@gmail.com>
2020-06-05 22:23:47 -0700

    adding a shell script ot reduce size of data

---
+++ mithun_scripts/reduce_size.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#this can be used as a toy data for quick end to end testing
+
+bert_format_base_folder_path="../src/transformers/data/datasets/fever"
+
+for complete_path in $(find $bert_format_base_folder_path -name '*.tsv');
+do
+head -100  $complete_path > temp
+mv temp $complete_path
+
+done
+
--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -5,30 +5,8 @@ export PYTHONPATH="../src/"
 export GLUE_DIR="../src/transformers/data/datasets/fever/feverindomain/lex/"
 export TASK_NAME=feverindomain
 
-#python ../examples/text-classification/run_glue.py \
-#    --model_name_or_path bert-base-uncased \
-#    --task_name $TASK_NAME \
-#    --do_train \
-#    --do_eval \
-#    --data_dir $GLUE_DIR \
-#    --max_seq_length 128 \
-#    --per_device_eval_batch_size=8   \
-#    --per_device_train_batch_size=8   \
-#    --learning_rate 2e-5 \
-#    --num_train_epochs 3.0 \
-#    --output_dir /tmp/$TASK_NAME \
-#    --cache_dir /tmp/cache/$TASK_NAME \
-#    --overwrite_output_dir \
-
 python ../examples/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval      --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir /tmp/$TASK_NAME --overwrite_output_dir
 
 
-
-#QNLI version for edit configurations in pycharm
-# --model_name_or_path bert-base-uncased     --task_name QNLI      --do_train     --do_eval      --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/QNLI/      --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir /tmp/QNLI/ --cache_dir /tmp/cache/qnli/
-
-#mnlI version for edit configurations in pycharm
-# --model_name_or_path bert-base-uncased     --task_name MNLI      --do_train     --do_eval      --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/MNLI/      --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir /tmp/MNLI/
-
-#run parameters for feverindomain
+# expanded run parameters for feverindomain
 # --model_name_or_path bert-base-uncased     --task_name feverindomain      --do_train     --do_eval      --data_dir /Users/mordor/research/huggingface/src/transformers/data/datasets/fever/feverindomain/lex/      --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir /tmp/feverindomain/ --overwrite_output_dir
