c4868481c4f58c9198281c3f78ca2b1b2cdc6a6e
mithun <mithunpaul08@gmail.com>
2020-06-05 22:28:33 -0700

    will return simple accuracy. no f1

--- src/transformers/data/metrics/__init__.py
+++ src/transformers/data/metrics/__init__.py
@@ -75,7 +75,7 @@ if _has_sklearn:
         elif task_name == "hans":
             return {"acc": simple_accuracy(preds, labels)}
         elif task_name == "feverindomain":
-            return acc_and_f1(preds, labels)
+            return {"acc": simple_accuracy(preds, labels)}
         else:
             raise KeyError(task_name)
 
