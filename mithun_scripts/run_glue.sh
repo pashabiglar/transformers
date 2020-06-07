4572516ea0971f6e23b36b7b3447d99297e932f6
mithun <mithunpaul08@gmail.com>
2020-06-06 18:54:05 -0700

    path to run_glue was wrong

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -9,7 +9,7 @@ mkdir -p /xdisk/msurdeanu/mithunpaul/huggingface/transformers/examples/text-clas
 
 export OUTPUT_DIR="/xdisk/msurdeanu/mithunpaul/huggingface/transformers/examples/text-classification/output"
 
-python /xdisk/msurdeanu/mithunpaul/huggingface/transformers/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval   --do_predict   --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir $OUTPUT_DIR --overwrite_output_dir
+python /xdisk/msurdeanu/mithunpaul/huggingface/transformers/examples/text-classification/run_glue.py  --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval   --do_predict   --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir $OUTPUT_DIR --overwrite_output_dir
 
 
 # expanded run parameters for feverindomain
