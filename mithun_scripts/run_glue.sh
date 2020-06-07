d517a9851c3a707ea4a451444154a4079787dc7a
mithun <mithunpaul08@gmail.com>
2020-06-07 15:00:50 -0700

    preb-

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -15,7 +15,7 @@ export TASK_NAME=fevercrossdomain
 
 
 #python /xdisk/msurdeanu/mithunpaul/huggingface/transformers/examples/text-classification/run_glue.py  --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval   --do_predict   --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 1.0      --output_dir $OUTPUT_DIR --overwrite_output_dir
-python3.6 ../examples/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train     --do_eval      --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 5.0      --output_dir /tmp/$TASK_NAME --overwrite_output_dir
+python3.6 ../examples/text-classification/run_glue.py --model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train  --do_predict --do_eval      --data_dir $GLUE_DIR    --max_seq_length 128      --per_device_eval_batch_size=8        --per_device_train_batch_size=8        --learning_rate 2e-5      --num_train_epochs 5.0      --output_dir /tmp/$TASK_NAME --overwrite_output_dir
 
 
 # expanded run parameters for feverindomain
