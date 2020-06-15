d0ae67ddb6d189805384b0107946c4b457cf60ba
mithun <mithunpaul08@gmail.com>
2020-06-14 23:40:34 -0700

    export PYTHONPATH=/home/u11/mithunpaul/huggingfacev2

--- mithun_scripts/run_on_hpc_ocelote_venv.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N fdf
+#PBS -N hf_teacher_student
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
