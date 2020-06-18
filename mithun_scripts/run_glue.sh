c59a5854b505d361975c0d757027ae8611258d4e
mithun <mithunpaul08@gmail.com>
2020-06-17 18:21:15 -0700

    will run oaner on hpc for toy data

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -31,10 +31,10 @@ echo "data_dir is $DATA_DIR"
 
 
 mkdir -p output
-#python3.6 ../examples/text-classification/run_glue.py $args
+python3.6 ../examples/text-classification/run_glue.py $args
 #for laptop run from terminal
 echo $args
-python3 ../examples/text-classification/run_glue.py $args
+#python3 ../examples/text-classification/run_glue.py $args
 
 # for pycharm
 #feverindomain
