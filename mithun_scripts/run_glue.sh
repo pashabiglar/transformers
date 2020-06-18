077154cab992df39de35b1f79ddbe053ed210630
mithun <mithunpaul08@gmail.com>
2020-06-17 17:30:56 -0700

    runs fine on local machine. still need to verify in code

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -9,7 +9,7 @@ export OUTPUT_DIR="output/"
 
 
 
-args="--model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train   --do_eval   --do_predict    --data_dir $DATA_DIR    --max_seq_length 128      --per_device_eval_batch_size=16        --per_device_train_batch_size=16       --learning_rate 1e-5      --num_train_epochs 10.0      --output_dir $OUTPUT_DIR --overwrite_output_dir  --save_steps 37000 --weight_decay 0.01 --adam_epsilon 1e-6"
+args="--model_name_or_path bert-base-uncased     --task_name $TASK_NAME      --do_train   --do_eval   --do_predict    --data_dir $DATA_DIR    --max_seq_length 128      --per_device_eval_batch_size=16        --per_device_train_batch_size=16       --learning_rate 1e-5      --num_train_epochs 1.0      --output_dir $OUTPUT_DIR --overwrite_output_dir  --save_steps 37000 --weight_decay 0.01 --adam_epsilon 1e-6"
 
 echo "TASK_TYPE is $TASK_TYPE"
 if [ "$TASK_TYPE" = "lex" ] ; then
@@ -31,9 +31,9 @@ echo "data_dir is $DATA_DIR"
 
 
 mkdir -p output
-python3.6 ../examples/text-classification/run_glue.py $args
+#python3.6 ../examples/text-classification/run_glue.py $args
 #for laptop run from terminal
-#python3 ../examples/text-classification/run_glue.py $args
+python3 ../examples/text-classification/run_glue.py $args
 
 # for pycharm
 #feverindomain
