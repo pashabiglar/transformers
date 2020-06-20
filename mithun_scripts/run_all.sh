495e290aca2ecf249e8f58452422776958dcf986
mithun <mithunpaul08@gmail.com>
2020-06-19 17:03:43 -0700

    will run oaner alone but for toy data 5 epochs seed=9843

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -2,7 +2,7 @@
 export DATA_DIR_BASE="../src/transformers/data/datasets"
 export DATASET="fever"
 export basedir="$DATA_DIR_BASE/$DATASET"
-export TASK_TYPE="combined" #options for task type include lex,delex,and empty"". Empty is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
+export TASK_TYPE="delex" #options for task type include lex,delex,and empty"". Empty is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE"
 #export PYTHONPATH="/Users/mordor/research/transformers/src"
@@ -10,7 +10,7 @@ export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE"
 #comment this section if you just downloaded and converted the data fresh using these.-useful for repeated runs
 rm -rf $basedir
 ./get_fever_fnc_data.sh
-#./reduce_size.sh
+./reduce_size.sh
 ./convert_to_mnli_format.sh
 #############end of commentable data sections
 ./run_glue.sh
