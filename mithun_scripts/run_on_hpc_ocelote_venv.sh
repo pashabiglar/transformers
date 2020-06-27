d356ef6eae12487a53186c4d7bf6ce99bc2e54cd
mithun <mithunpaul08@gmail.com>
2020-06-26 18:36:27 -0700

    changing hardcoded folder name tuning_huggingface

--- mithun_scripts/run_on_hpc_ocelote_venv.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv.sh
@@ -46,7 +46,7 @@ pip install --upgrade pip
 
 
 #one which worked at noon june 6th 2020
-cd /home/u11/mithunpaul/huggingfacev2/mithun_scripts/
+cd /home/u11/mithunpaul/tuning_huggingface/mithun_scripts/
 pip install -r requirements.txt
 #export PYTHONPATH="/xdisk/msurdeanu/mithunpaul/huggingface//transformers/src/"
 
