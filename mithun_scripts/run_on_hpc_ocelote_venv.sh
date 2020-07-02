d366fbc3d353b306dc43c3e7b794eaf2ce422bcf
mithun <mithunpaul08@gmail.com>
2020-07-02 15:22:10 -0700

    manually installed wandb on hpc venv. pushing again to run on toy data

--- mithun_scripts/run_on_hpc_ocelote_venv.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv.sh
@@ -25,7 +25,7 @@ module load python/3.6/3.6.5
 #uncomment this if you don't want to reinstall venv- usually you just have to do this only once ever
 #rm -rf my_virtual_env
 #mkdir my_virtual_env
-python3 -m venv my_virtual_env
+#python3 -m venv my_virtual_env
 
 #this is the only line you need if you already have a virtual_env set up
 source my_virtual_env/bin/activate
@@ -38,18 +38,9 @@ pip install --upgrade pip
 
 #####my code part
 
-#pip install -r /xdisk/msurdeanu/mithunpaul/huggingface/transformers/examples/requirements.txt
-#
-#cd /xdisk/msurdeanu/mithunpaul/huggingface/transformers/mithun_scripts/
-#bash /xdisk/msurdeanu/mithunpaul/huggingface/transformers/mithun_scripts/run_all.sh
-
-
-
-#one which worked at noon june 6th 2020
 cd /home/u11/mithunpaul/huggingfacev2/mithun_scripts/
-pip install -r requirements.txt
-pip install wandb; wandb login
-exit
+#pip install -r requirements.txt
+
 
 bash run_all.sh
 
