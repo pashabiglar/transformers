280438630cf36093a364b72d8781d0336134e3de
mithun <mithunpaul08@gmail.com>
2020-07-02 21:40:26 -0700

    commenting out wandb install in shell svript

--- mithun_scripts/run_on_hpc_ocelote_venv.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv.sh
@@ -23,15 +23,15 @@ module load cuda90/neuralnet/7/7.3.1.20
 module load python/3.6/3.6.5
 
 #uncomment this if you don't want to reinstall venv- usually you just have to do this only once ever
-rm -rf my_virtual_env
-mkdir my_virtual_env
-python3 -m venv my_virtual_env
+#rm -rf my_virtual_env
+#mkdir my_virtual_env
+#python3 -m venv my_virtual_env
 
 #this is the only line you need if you already have a virtual_env set up
 source my_virtual_env/bin/activate
 
 export PYTHONPATH="/home/u11/mithunpaul/huggingfacev2/src"
-pip install --upgrade pip
+#pip install --upgrade pip
 #pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92-f https://download.pytorch.org/whl/torch_stable.html
 
 
@@ -39,11 +39,11 @@ pip install --upgrade pip
 #####my code part
 
 cd /home/u11/mithunpaul/huggingfacev2/mithun_scripts/
-pip install -r requirements.txt
-pip install wandb
-WANDB_API_KEY=de268c256c2d4acd9085ee4e05d91706c49090d7
-wandb login
-export WANDB_NAME="from_hpc"
+#pip install -r requirements.txt
+#pip install wandb
+#WANDB_API_KEY=de268c256c2d4acd9085ee4e05d91706c49090d7
+#wandb login
+#export WANDB_NAME="from_hpc"
 bash run_all.sh
 
 
