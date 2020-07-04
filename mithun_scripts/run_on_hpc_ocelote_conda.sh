c076e519915b11337c54b119dc44f9d3dea8ae16
mithun <mithunpaul08@gmail.com>
2020-07-04 01:50:21 -0700

    wont create conda. will just source activate

--- mithun_scripts/run_on_hpc_ocelote_conda.sh
+++ mithun_scripts/run_on_hpc_ocelote_conda.sh
@@ -27,14 +27,14 @@ module load cuda90/neuralnet/7/7.3.1.20
 module load python/3.6/3.6.5
 module load anaconda/2020/2020.02
 
-conda create --name huggingface2 python=3.8
+#conda create --name huggingface2 python=3.8
 
 #this is the only line you need if you already have a virtual_env set up
 source activate huggingface2
 
 
 
-#pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92-f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.5.0+cu92 torchvision==0.6.0+cu92-f https://download.pytorch.org/whl/torch_stable.html
 
 
 
@@ -44,6 +44,9 @@ cd /home/u11/mithunpaul/huggingfacev2/mithun_scripts/
 export PYTHONPATH="/home/u11/mithunpaul/huggingfacev2/src"
 pip install --upgrade pip
 pip install wandb
+pip install matplotlib
+pip install pandas
+pip install filelock
 pip install -r requirements.txt
 
 WANDB_API_KEY=de268c256c2d4acd9085ee4e05d91706c49090d7
