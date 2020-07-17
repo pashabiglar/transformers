da671af8155ccb1d42ce51d8faab0038722586a8
mithun <mithunpaul08@gmail.com>
2020-07-17 15:28:13 -0700

    previuous commit gave error that dataclasses not found. this usually happens when we run on clara using python3.6 command. replacing that with just python, which i think runs python3.8 on clara by default

--- mithun_scripts/run_glue.sh
+++ mithun_scripts/run_glue.sh
@@ -35,7 +35,7 @@ echo "data_dir is $DATA_DIR"
 
 
 mkdir -p output
-python3.6 ../examples/text-classification/run_glue.py $args
+python ../examples/text-classification/run_glue.py $args
 #for laptop run from terminal
 echo $args
 
