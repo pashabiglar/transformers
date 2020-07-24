5e7bd5550fc10d65acc820ffaf4d9bd27f735ee3
mithun <mithunpaul08@gmail.com>
2020-07-24 14:42:27 -0700

    17 epochs

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -38,7 +38,7 @@ pip install --upgrade pip
 cd /home/u11/mithunpaul/xdisk/huggingface_bert/code/mithun_scripts/
 pip install -r requirements.txt
 
-bash run_all.sh --epochs_to_run 16 --machine_to_run_on server
+bash run_all.sh --epochs_to_run 17 --machine_to_run_on server
 
 
 
