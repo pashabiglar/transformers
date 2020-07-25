bf065e91a8fbffa1d723b2c6b4f769397c2838c8
mithun <mithunpaul08@gmail.com>
2020-07-25 14:03:23 -0700

    so previous commits on hpc finished running delex figer till 23 epochs. epochs 24 and 25  have been stuck on windfall mode for a long time.I killed them. Instead i am starting experiments with lex_bert cased. Have made the vocab link to point to aws original vocab. will run for 1 epoch

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -43,7 +43,7 @@ echo $EPOCHS
 
 export DATASET="fever"
 export basedir="$DATA_DIR_BASE/$DATASET"
-export TASK_TYPE="delex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
+export TASK_TYPE="lex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
 export SUB_TASK_TYPE="figerspecific" #options for TASK_SUB_TYPE (usually used only for delex)  include [oa, figerspecific, figerabstract, oass, simplener]
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE"
