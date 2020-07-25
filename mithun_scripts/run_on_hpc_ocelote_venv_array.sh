cec41705adfdb0f6b2d08887e1e8f4d88cfe7f93
mithun <mithunpaul08@gmail.com>
2020-07-24 18:16:47 -0700

    delx figer bert-base-cased with custom uofa made vocab + 23 epochs+ 15 hours wall time

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,13 +3,13 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N delex_bert_cased_figer_19epochs
+#PBS -N delex_bert_cased_figer_23epochs
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### Walltime is how long your job will run
-#PBS -l walltime=20:00:00
+#PBS -l walltime=15:00:00
 ### Joins standard error and standard out
 #PBS -j oe
 
