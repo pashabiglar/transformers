666e403e0227745c57061361f47e341d54814cb1
mithun <mithunpaul08@gmail.com>
2020-07-28 13:38:16 -0700

    the tokenization is still happening, even after more than 16 hours later. now i am going to remove overwrite_cache from training arguments. I think that was forcing it to create a tokenization cache every time. Instead i will copy out the cache from older runs

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -27,7 +27,7 @@ fi
 
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
         export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fix_parallelism_per_epoch_issue/output"
-        export DATA_DIR_BASE="//home/u11/mithunpaul/xdisk/huggingface_bert_fix_parallelism_per_epoch_issue/data"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fix_parallelism_per_epoch_issue/data"
 else
         export DATA_DIR_BASE="../src/transformers/data/datasets"
         export OUTPUT_DIR_BASE="output"
