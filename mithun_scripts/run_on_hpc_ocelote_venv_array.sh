7269614a65233e48ee2a0d39ca0a5682abbbbe94
mithun <mithunpaul08@gmail.com>
2020-07-28 00:23:27 -0700

    reducing to 23 hours since they said will run into maintenace time.wtf

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -9,7 +9,7 @@
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### Walltime is how long your job will run
-#PBS -l walltime=48:00:00
+#PBS -l walltime=23:00:00
 ### Joins standard error and standard out
 #PBS -j oe
 
