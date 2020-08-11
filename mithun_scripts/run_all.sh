48d75901587fcd9cef5ca18e86719dffdfc05cdb
mithun <mithunpaul08@gmail.com>
2020-08-10 19:11:11 -0700

    after bug fix no tokenization. should hopefully run 25 epochs delex bert cased with our custom vocab and not be stuck in tokenization loop

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -41,12 +41,12 @@ echo $EPOCHS
 
 export DATASET="fever"
 export basedir="$DATA_DIR_BASE/$DATASET"
-export TASK_TYPE="lex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
+export TASK_TYPE="delex" #options for task type include lex,delex,and combined"". combined is used in case of student teacher architecture which will load a paralleldataset from both lex and delex folders
 export SUB_TASK_TYPE="figerspecific" #options for TASK_SUB_TYPE (usually used only for delex)  include [oa, figerspecific, figerabstract, oass, simplener]
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE"
 export PYTHONPATH="../src"
-export BERT_MODEL_NAME="bert-base-uncased" #options include things like [bert-base-uncased,bert-base-cased] etc. refer src/transformers/tokenization_bert.py for more.
+export BERT_MODEL_NAME="bert-base-cased" #options include things like [bert-base-uncased,bert-base-cased] etc. refer src/transformers/tokenization_bert.py for more.
 export MAX_SEQ_LENGTH="128"
 export OUTPUT_DIR="$OUTPUT_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE/$BERT_MODEL_NAME/$MAX_SEQ_LENGTH/"
 echo $OUTPUT_DIR
@@ -60,7 +60,7 @@ echo $OUTPUT_DIR
 if [ $EPOCHS = "1" ]; then
         echo "found epopch is equal to 1. going to download data"
         #rm -rf $DATA_DIR
-        #./get_fever_fnc_data.sh
+        ./get_fever_fnc_data.sh
         #./convert_to_mnli_format.sh
 fi
 
