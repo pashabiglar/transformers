d4cfdc4ba84ec80159adde3c55065462bdd96b40
mithun <mithunpaul08@gmail.com>
2020-08-10 19:05:17 -0700

    removed force_download is true. if data file is already downloaded, will not do tokenization again. This should fix the tokenization issue

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -59,9 +59,9 @@ echo $OUTPUT_DIR
 #get data only if its 1st epoch
 if [ $EPOCHS = "1" ]; then
         echo "found epopch is equal to 1. going to download data"
-        rm -rf $DATA_DIR
-        ./get_fever_fnc_data.sh
-        ./convert_to_mnli_format.sh
+        #rm -rf $DATA_DIR
+        #./get_fever_fnc_data.sh
+        #./convert_to_mnli_format.sh
 fi
 
 echo "done with data download part if epoch==1. datapath now is $DATA_DIR"
