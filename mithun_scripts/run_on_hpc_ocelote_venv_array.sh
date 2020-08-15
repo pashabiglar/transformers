0763a99de3fbf4f774486629be584971f1015488
mithun <mithunpaul08@gmail.com>
2020-08-15 01:01:03 -0700

    cache ran again. so code was trying to run 120k data again

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -39,7 +39,7 @@ pip install -r requirements.txt
 
 cd /home/u11/mithunpaul/xdisk/huggingface_bert_master/code/mithun_scripts
 
-bash run_all.sh --epochs_to_run 2 --machine_to_run_on hpc #options include [laptop, hpc,clara]
+bash run_all.sh --epochs_to_run 1 --machine_to_run_on hpc #options include [laptop, hpc,clara]
 
 
 
