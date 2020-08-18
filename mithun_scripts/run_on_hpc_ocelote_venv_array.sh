d1386775a98cd2cfc2bf02a446511ff8a9aca7bc
mithun <mithunpaul08@gmail.com>
2020-08-17 20:08:33 -0700

    testing student teacher branch again for 100data points to check if it still reproduces results

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N master_branch_lex_cased_figers_128
+#PBS -N student_teacher_branch_lex_cased_figers_128
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
