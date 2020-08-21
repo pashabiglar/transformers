376dcac2ebbb8014e507b41e0da63e71255abeb3
mithun <mithunpaul08@gmail.com>
2020-08-20 21:12:29 -0700

    will run 1 epoch for lex cased to download data

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -68,7 +68,6 @@ fi
 echo "done with data download part if epoch==1. datapath now is $DATA_DIR"
 
 
- ./reduce_size.sh  --data_path $DATA_DIR
 if [ $MACHINE_TO_RUN_ON == "laptop" ]; then
       ./reduce_size.sh  --data_path $DATA_DIR
 fi
