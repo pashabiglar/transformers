5757857c9b614b5d18664ce6aa26427148b97b85
mithun <mithunpaul08@gmail.com>
2020-09-07 18:17:32 -0700

    testing on dev and test partition was being done using student model, even though we were training teacher. fied. will run for 9k data points first

--- mithun_scripts/reduce_size.sh
+++ mithun_scripts/reduce_size.sh
@@ -20,7 +20,7 @@ do
 
 
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
-    head -10000  $complete_path > temp
+    head -9000  $complete_path > temp
     mv temp $complete_path
 else
     head -10  $complete_path > temp
