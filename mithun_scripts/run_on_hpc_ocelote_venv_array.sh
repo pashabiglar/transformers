06bdc48aab63889ff25e4fcc1ded6afa46627f27
mithun <mithunpaul08@gmail.com>
2020-09-10 12:56:39 -0700

    changing requested time to 30 hours

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,13 +3,13 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N run_full_student_teacher_bert_cased_figer_specific_128
+#PBS -N run_full_student_teacher_bert_cased_figer_specific_128_consistencyLossWeight0.5
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
 #PBS -l place=pack:shared
 ### Walltime is how long your job will run
-#PBS -l walltime=25:00:00
+#PBS -l walltime=30:00:00
 ### Joins standard error and standd out
 #PBS -j oe
 
