5eaccdffa4652169df289658578e1b3868c1406e
mithun <mithunpaul08@gmail.com>
2020-09-16 17:58:38 -0700

    name change of pbs- n

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N run_full_student_teacher_bert_cased_figer_specific_128_consistencyLossWeight0.5
+#PBS -N tuning_student_teacher
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
