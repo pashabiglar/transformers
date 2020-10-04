fff92f9824b7b0cce7d10d680e40216d3c79aa16
mithun <mithunpaul08@gmail.com>
2020-10-03 18:39:54 -0700

    again changed my mind about folder name. decided will include the tuning factor also, so that we can run tuning out of multiple folders at the same time. hopefully wont be a scheduling nightmare

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -29,13 +29,13 @@ python3 -m venv my_virtual_env
 #this is the only line you need if you already have a virtual_env set up
 source my_virtual_env/bin/activate
 
-export PYTHONPATH="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher/code/src"
+export PYTHONPATH="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt025/code/src"
 export CUDA_VISIBLE_DEVICES=0
 
 pip install --upgrade pip
 
    
-cd /home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher/code/examples
+cd /home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt025/code/examples
 
 pip install -r requirements.txt
 pip install transformers
