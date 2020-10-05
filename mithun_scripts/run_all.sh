767685e1a955fb9a3309666fa4fce8ffee4e3740
mithun <mithunpaul08@gmail.com>
2020-10-05 13:49:49 -0700

    so previous two commits ran good. the one with learning rate of 0.0875 can be found in a graph called legendary-voice-1016 . that gave a highest cross domain accuracy of 74.74 and fncscore of 63.44 at epoch 2. so looks like tuning learning rate is helping the cause of cross domain fnc score. however, changing the dropout to 0.5 from 0.1 was a disaster. all scores went back to around50 percentage. those results can be seen in a wandb graph called playful-salad-1015. anyway, this comming i am going tback to drop out of 0.1 (the bert default) also have changed learning rate to 0.090. also changed folder name huggingface_bert_tuning_student_teacher_lr0pt090

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -39,8 +39,8 @@ fi
 
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
 
-        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_dropout0pt5/output"
-        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_dropout0pt5/data"
+        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt090/output"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt090/data"
 else
         wandb off
         export DATA_DIR_BASE="/Users/mordor/research/huggingface/src/transformers/data/datasets"
