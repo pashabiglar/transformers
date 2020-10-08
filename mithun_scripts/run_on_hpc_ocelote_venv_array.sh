a0f3e5e43c1f4b7f34aa6df027411a372261de0f
mithun <mithunpaul08@gmail.com>
2020-10-07 20:49:58 -0700

    changing folder paths to the new folder in which this will be run via huggingface_bert_ema

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N train_delex
+#PBS -N huggingface_bert_ema
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
@@ -29,19 +29,19 @@ python3 -m venv my_virtual_env
 #this is the only line you need if you already have a virtual_env set up
 source my_virtual_env/bin/activate
 
-export PYTHONPATH="/home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt025/code/src"
+export PYTHONPATH="/home/u11/mithunpaul/xdisk/huggingface_bert_ema/code/src"
 export CUDA_VISIBLE_DEVICES=0
 
 pip install --upgrade pip
 
    
-cd /home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt025/code/examples
+cd /home/u11/mithunpaul/xdisk/huggingface_bert_ema/code/examples
 
 pip install -r requirements.txt
 pip install transformers
 pip install wget
 
-cd /home/u11/mithunpaul/xdisk/huggingface_bert_tuning_student_teacher_lr0pt025/code/mithun_scripts
+cd /home/u11/mithunpaul/xdisk/huggingface_bert_ema/code/mithun_scripts
 
 bash run_all.sh --epochs_to_run 25 --machine_to_run_on hpc --use_toy_data false #options include [laptop, hpc,clara]
 #stub to use in laptop
