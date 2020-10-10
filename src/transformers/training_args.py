656afebd8cec7066ffab7f4ebdb03c42dd0e92fb
mithun <mithunpaul08@gmail.com>
2020-10-09 21:54:39 -0700

    huggingface_bert_tuning_student_teacher_seed95635

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -215,7 +215,7 @@ class TrainingArguments:
         },
     )
     no_cuda: bool = field(default=False, metadata={"help": "Do not use CUDA even when it is available"})
-    seed: int = field(default=5075, metadata={"help": "random seed for initialization"})
+    seed: int = field(default=95635, metadata={"help": "random seed for initialization"})
 
     fp16: bool = field(
         default=False,
