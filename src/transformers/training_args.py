4cafe5472a6633cb571284b9691dac30bed32dea
mithun <mithunpaul08@gmail.com>
2020-10-11 22:04:16 -0700

    huggingface_bert_tuning_delex_from_inside_student_teacher_seed39

--- src/transformers/training_args.py
+++ src/transformers/training_args.py
@@ -215,7 +215,7 @@ class TrainingArguments:
         },
     )
     no_cuda: bool = field(default=False, metadata={"help": "Do not use CUDA even when it is available"})
-    seed: int = field(default=6, metadata={"help": "random seed for initialization"})
+    seed: int = field(default=39, metadata={"help": "random seed for initialization"})
 
     fp16: bool = field(
         default=False,
