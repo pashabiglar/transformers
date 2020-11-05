9e8613a6af845a83b210518316717ac93546efa2
mithun <mithunpaul08@gmail.com>
2020-11-04 17:34:58 -0700

    with that double percentage i pass the argument parser, but now google says it doenst know what my%20drive means...replacing it with \space this time...smh

--- examples/text-classification/config_combined_cased_load_and_test_trained_model_light_plasma_886.py
+++ examples/text-classification/config_combined_cased_load_and_test_trained_model_light_plasma_886.py
@@ -5,7 +5,7 @@ do_train=True
 do_eval=True
 do_predict=True
 do_train_1student_1teacher=True
-data_dir="/content/gdrive/My\ 20Drive/colab_fall2020/data/fever/fevercrossdomain/combined/figerspecific/toydata/"
+data_dir="/content/gdrive/My\ Drive/colab_fall2020/data/fever/fevercrossdomain/combined/figerspecific/toydata/"
 max_seq_length="128"
 per_device_eval_batch_size="16"
 per_device_train_batch_size="16"
@@ -13,7 +13,7 @@ learning_rate="1e-5"
 # Note: use num_train_epochs=1 only. For testing purposes, in trainery.py we are as of now, sep 2020 we are returning the dev and test partition evaluation results for epoch1
 # if you want to test for more than 1 eopch, return best_dev and best_test values
 num_train_epochs="1"
-output_dir="/content/gdrive/My\ 20Drive/colab_fall2020/outputs/fever/fevercrossdomain/combined/figerspecific/bert-base-cased/128/"
+output_dir="/content/gdrive/My\ Drive/colab_fall2020/outputs/fever/fevercrossdomain/combined/figerspecific/bert-base-cased/128/"
 overwrite_output_dir=True
 weight_decay="0.01"
 adam_epsilon="1e-6"
@@ -21,7 +21,7 @@ evaluate_during_training=True
 task_type="combined"
 subtask_type="figerspecific"
 machine_to_run_on="laptop"
-toy_data_dir_path="/content/gdrive/My\ 20Drive/colab_fall2020/data/fever/fevercrossdomain/combined/figerspecific/toydata/"
+toy_data_dir_path="/content/gdrive/My\ Drive/colab_fall2020/data/fever/fevercrossdomain/combined/figerspecific/toydata/"
 #what are the scores that you should assert against. i.e the ones we got when we ran the toy data alone
 fever_in_domain_accuracy_on_toy_data_17_datapoints=0.9375
 fever_cross_domain_accuracy_on_toy_data_17_datapoints=0.875
