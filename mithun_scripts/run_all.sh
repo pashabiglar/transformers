9eb78a551fd2f7db84c4ed9026683e21b53b6930
mithunpaul08 <mithunpaul08@gmail.com>
2021-01-26 14:24:11 -0700

    changing folder name to huggingface_bert_fever_to_fnc_run_training_3models_classweight00875

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -45,8 +45,8 @@ fi
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
 
 
-        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fever_to_fnc_run_training_3models/output"
-        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fever_to_fnc_run_training_3models/data"
+        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fever_to_fnc_run_training_3models_classweight00875/output"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/huggingface_bert_fever_to_fnc_run_training_3models_classweight00875/data"
 else
         wandb off
         export DATA_DIR_BASE="/Users/mordor/research/huggingface/src/transformers/data/datasets"
