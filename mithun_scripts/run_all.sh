07b9264b445ebb655fc7ceb27321d5686740bf62
mithunpaul08 <mithunpaul08@gmail.com>
2021-01-27 22:44:47 -0700

    fuck this shit. not able to figure out how to allocate cuda. will just try with google/bert_uncased_L-12_H-128_A-2 directly now

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -70,7 +70,7 @@ export SUB_TASK_TYPE="figerspecific" #options for TASK_SUB_TYPE (usually used on
 export TASK_NAME="fevercrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE"
 export PYTHONPATH="../src"
-export BERT_MODEL_NAME="bert-base-cased" #options include things like [bert-base-uncased,bert-base-cased] etc. refer src/transformers/tokenization_bert.py for more.
+export BERT_MODEL_NAME="google/bert_uncased_L-12_H-128_A-2" #options include things like [bert-base-uncased,bert-base-cased] etc. refer src/transformers/tokenization_bert.py for more.
 export MAX_SEQ_LENGTH="128"
 export OUTPUT_DIR="$OUTPUT_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE/$SUB_TASK_TYPE/$BERT_MODEL_NAME/$MAX_SEQ_LENGTH/"
 echo $OUTPUT_DIR
