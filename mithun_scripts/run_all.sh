e0c7ad90df03f91d13a7f9b160d2937744d858e1
mithunpaul08 <mithunpaul08@gmail.com>
2021-01-29 20:46:27 -0700

    will run 3t1s for 55 epochs weight_classification_loss = 2+export CUDA_VISIBLE_DEVICES=0+learning rate=10e-5+will run froim folder huggingface_bertmini_1t1s_v1

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -56,8 +56,8 @@ fi
 
 if [ $MACHINE_TO_RUN_ON == "clara" ]; then
         wandb online
-        export OUTPUT_DIR_BASE="/work/mithunpaul/huggingface_bertmini_multiple_teachers_v5/output"
-        export DATA_DIR_BASE="/work/mithunpaul/huggingface_bertmini_multiple_teachers_v5/data"
+        export OUTPUT_DIR_BASE="/work/mithunpaul/huggingface_bertmini_multiple_teachers_v1/output"
+        export DATA_DIR_BASE="/work/mithunpaul/huggingface_bertmini_multiple_teachers_v1/data"
 fi
 
 
