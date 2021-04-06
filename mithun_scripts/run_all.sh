8be3739e797491f85b63a5cf2473fe1f8ae41dca
mithunpaul08 <mithunpaul08@gmail.com>
2021-04-05 19:33:06 -0700

    previous commit succesfully started run for groupshot with 2 data points. now will do with 10 data points for each model...i dont know if overall it must be called 40 or 10...i still think its 10 with 4 different views of it

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -44,8 +44,8 @@ fi
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
         wandb on
         wandb online
-        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_10fewshot/output"
-        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_10fewshot/data"
+        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fewshot/output"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fewshot/data"
 fi
 
 
