3228bc222773f4e136cf4127e19e2a60cc29bd54
mithunpaul08 <mithunpaul08@gmail.com>
2021-04-07 13:43:34 -0700

    fewshot_lexStandAlone_fever2fnc_200datapoints

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -44,8 +44,8 @@ fi
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
         wandb on
         wandb online
-        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/fewshot_lexStandAlone_fever2fnc_100datapoints/output"
-        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/fewshot_lexStandAlone_fever2fnc_100datapoints/data"
+        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/fewshot_lexStandAlone_fever2fnc_200datapoints/output"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/fewshot_lexStandAlone_fever2fnc_200datapoints/data"
 fi
 
 
