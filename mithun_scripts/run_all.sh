b177258fc1f2e8f69405d7aef7594c75839bba9a
mithunpaul08 <mithunpaul08@gmail.com>
2021-04-08 00:20:57 -0700

    group_learning_fever2fnc_lr10e5_classweight2_fewshot_200datapoints

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -44,8 +44,8 @@ fi
 if [ $MACHINE_TO_RUN_ON == "hpc" ]; then
         wandb on
         wandb online
-        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fever2fnc_lr10e5_classweight2_fewshot_50datapoints/output"
-        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fever2fnc_lr10e5_classweight2_fewshot_50datapoints/data"
+        export OUTPUT_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fever2fnc_lr10e5_classweight2_fewshot_200datapoints/output"
+        export DATA_DIR_BASE="/home/u11/mithunpaul/xdisk/group_learning_fever2fnc_lr10e5_classweight2_fewshot_200datapoints/data"
 fi
 
 
