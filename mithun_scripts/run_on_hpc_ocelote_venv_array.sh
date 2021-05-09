cee10d30aa33a7fbfbadb7c4ed34e42adff0b38a
mithunpaul08 <mithunpaul08@gmail.com>
2021-05-08 17:17:55 -0700

    lexStandAlone_fever2fnc_bert_large_uncased

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N bert_large_cased
+#PBS -N uncased_bert_large
 
 ### Specify the group name
 #PBS -W group_list=msurdeanu
@@ -34,7 +34,7 @@ module load python/3.6/3.6.5
 source my_virtual_env/bin/activate
 
 
-export PYTHONPATH="/home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_cased/code/src"
+    export PYTHONPATH="/home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_uncased/code/src"
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -45,7 +45,7 @@ python -m spacy download en_core_web_sm
 
    
 
-cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_cased/code/examples
+cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_uncased/code/examples
 
 pip install -r requirements.txt
 pip install transformers
@@ -53,7 +53,7 @@ pip install wget
 pip install stop-words  --no-cache-dir
 
 
-cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_cased/code/mithun_scripts
+cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_large_uncased/code/mithun_scripts
 
 bash run_all.sh --epochs_to_run 55 --machine_to_run_on hpc --use_toy_data false --download_fresh_data true #options include [laptop, hpc,clara]
 
