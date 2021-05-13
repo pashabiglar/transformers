502f7ac95714d797905ccbc3889ff89b25be20b6
mithunpaul08 <mithunpaul08@gmail.com>
2021-05-13 01:00:37 -0700

    lexStandAlone_fever2fnc_bert_base_cased_rs9348

--- mithun_scripts/run_on_hpc_ocelote_venv_array.sh
+++ mithun_scripts/run_on_hpc_ocelote_venv_array.sh
@@ -3,7 +3,7 @@
 #PBS -q standard
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1:os7=True
 ### Specify a name for the job
-#PBS -N 903rs_lex
+#PBS -N 9348rs_lex
 ### Specify the group name
 #PBS -W group_list=msurdeanu
 ### Used if job requires partial node only
@@ -33,7 +33,7 @@ module load python/3.6/3.6.5
 source my_virtual_env/bin/activate
 
 
-    export PYTHONPATH="/home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs903/code/src"
+    export PYTHONPATH="/home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs9348/code/src"
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -44,7 +44,7 @@ python -m spacy download en_core_web_sm
 
    
 
-cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs903/code/examples
+cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs9348/code/examples
 
 pip install -r requirements.txt
 pip install transformers
@@ -52,7 +52,7 @@ pip install wget
 pip install stop-words  --no-cache-dir
 
 
-cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs903/code/mithun_scripts
+cd /home/u11/mithunpaul/xdisk/lexStandAlone_fever2fnc_bert_base_cased_rs9348/code/mithun_scripts
 
 bash run_all.sh --epochs_to_run 55 --machine_to_run_on hpc --use_toy_data false --download_fresh_data true #options include [laptop, hpc,clara]
 
