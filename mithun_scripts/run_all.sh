d9b8b9d4b70bedb1bddf34dfef881a2f7b056a1d
mithunpaul08 <mithunpaul08@gmail.com>
2021-05-16 10:50:44 -0700

    lexStandAlone_fnc2fever_bert_base_cased_rs8939

--- mithun_scripts/run_all.sh
+++ mithun_scripts/run_all.sh
@@ -85,7 +85,7 @@ export TASK_TYPE="lex" #[lex, delex, combined, 3t1s]
 export TASK_NAME="fnccrossdomain" #options for TASK_NAME  include fevercrossdomain,feverindomain,fnccrossdomain,fncindomain
 export BERT_MODEL_NAME="bert-base-cased" #options include things like [bert-base-uncased,bert-base-cased , minibert(google/bert_uncased_L-12_H-128_A-2)] etc. refer src/transformers/tokenization_bert.py for more.
 export MAX_SEQ_LENGTH="128"
-
+   
 export basedir="$DATA_DIR_BASE/$DATASET"
 export PYTHONPATH="../src"
 export DATA_DIR="$DATA_DIR_BASE/$DATASET/$TASK_NAME/$TASK_TYPE"
